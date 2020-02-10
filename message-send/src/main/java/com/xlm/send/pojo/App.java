fd8f16fe53433050fff8b73a41cf252369829db8
姜沅初 <jiang_yc@suixingpay.com>
2020-02-10 11:46:32 +0800

    实体类使用lombok

--- message-send/src/main/java/com/xlm/send/pojo/App.java
+++ message-send/src/main/java/com/xlm/send/pojo/App.java
@@ -2,9 +2,11 @@ package com.xlm.send.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- message-send/src/main/java/com/xlm/send/pojo/Model.java
+++ message-send/src/main/java/com/xlm/send/pojo/Model.java
@@ -2,11 +2,13 @@ package com.xlm.send.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.util.Date;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
